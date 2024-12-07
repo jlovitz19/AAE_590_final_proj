55997a34717d1ad56866e6ee7aa6a1816bf3738f
Jonah Lovitz <129468112+jlovitz19@users.noreply.github.com>
2024-12-06 21:35:21 -0500

    Update monte_carlo_simulation_R.m

--- monte_carlo_simulation_R.m
+++ monte_carlo_simulation_R.m
@@ -1,17 +1,23 @@
-function R_k_hat = monte_carlo_simulation_R()
+function [v_k, R_k_hat] = monte_carlo_simulation_R()
+    % Performs Monte Carlo simulation to find the 
+    % measurement covariance matrix
+    %
+    % Parameters: none
+    %
+    % Outputs:
+    % v_k: Gaussian distributed measurement noise
+    % P_k_hat: a (supposedly) good estimate for 
+    % measurement covariance matrix
+
     N = 10;
 
-    sum_noise = zeros(4, 4);
+    sum_noise = zeros(6, 6);
 
     for i = 1:N
-        x0 = [389; 2.4; 0.056; 0.002; 0.022; 0.001]; % + dot([sqrt(sqrt(389)); sqrt(sqrt(2.4)); sqrt(sqrt(0.056)); sqrt(sqrt(0.002)); sqrt(sqrt(0.022)); sqrt(sqrt(0.001))], randn(6, 1));
-        v_k = [1e-2; 1e-8; 0.2; 0.3] + sqrt(0.04)*randn(4, 1);
-        w_k = [1e-4; 2e-4; 1e-5; 1e-4; 1e-4; 2e-4]  + sqrt(0.008)*randn(6, 1);
+        x0 = [389; 2.4; 0.056; 0.002; 0.022; 0.001]; 
+        v_k = [8*randn(1); 0.2*randn(1); 0.005*randn(1); 0.0002*randn(1); 0.0022*randn(1); 0.0001*randn(1)]; 
 
         [~, ~, y_k] = monte_sim_sample(v_k, x0);
-
-        %y_N(:, i) = y_k;
-        disp(y_k*y_k.');
 
         sum_noise = sum_noise + y_k*y_k.';
     end
