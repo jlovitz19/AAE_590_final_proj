01d8a5045a0f81de5d4684abadc82cb32227ffa4
Jonah Lovitz <129468112+jlovitz19@users.noreply.github.com>
2024-12-06 21:24:54 -0500

    Update EKF_update.m

--- EKF_update.m
+++ EKF_update.m
@@ -1,4 +1,4 @@
-function [x_k_k, P_k_k, y_k] = EKF_update(x_k_km1, P_k_km1, z_k, meas_noise)
+function [x_k_k, P_k_k, y_k] = EKF_update(x_k_km1, P_k_km1, z_k, R_k)
     % Performs the prediction step of the Extended Kalman Filter
     %
     % Parameters:
@@ -6,24 +6,23 @@ function [x_k_k, P_k_k, y_k] = EKF_update(x_k_km1, P_k_km1, z_k, meas_noise)
     % P_km1_km1: state covariance at previous step
     % k: current time step
     % z_k: measurement over whole timelapse
+    % v_k: 
     %
     % Outputs:
     % x_k_km1: a preori state estimate at current time step
     % P_km1_km1: a preori state covariance at current time step
-    % y_k: something
+    % y_k: measurement residual
+    % R_k: measurement noise covariance
 
-    %% todo: noises %%
 
     % compute measurement residual
     H_k = calc_measurement_jacobian(x_k_km1);
-  
-    R_k = meas_noise;
     
-    z_k_hat = H_k*x_k_km1(:, end); % + v_k; 
+    z_k_hat = H_k*x_k_km1(:, end);
     measurement_residual = z_k - z_k_hat; % measurement error
 
     % residual covariance and kalman gain
-    S_k = H_k*P_k_km1*H_k.' + R_k; % diag([chol(1e-32, "lower"), chol(1e-32, "lower"), chol(1e-32, "lower"), chol(1e-32, "lower")]); % + R_k;
+    S_k = H_k*P_k_km1*H_k.' + R_k; 
     K_k = P_k_km1*H_k.'*inv(S_k);
 
     % final estimations
