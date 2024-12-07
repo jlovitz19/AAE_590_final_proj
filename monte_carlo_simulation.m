6d5bd585cd74972c8cfd85dae0589b00a4851569
Jonah Lovitz <129468112+jlovitz19@users.noreply.github.com>
2024-12-06 21:33:42 -0500

    Update monte_carlo_simulation.m

--- monte_carlo_simulation.m
+++ monte_carlo_simulation.m
@@ -1,4 +1,11 @@
 function P_k_hat = monte_carlo_simulation()
+    % Performs Monte Carlo simulation to find the covariance matrix
+    %
+    % Parameters: none
+    %
+    % Outputs:
+    % P_k_hat: a (supposedly) good estimate for covariance matrix
+    
     N = 10;
 
     P_N = NaN(6, 6, N);
@@ -6,9 +13,8 @@ function P_k_hat = monte_carlo_simulation()
     sum = zeros(6, 6);
 
     for i = 1:N
-        x0 = [389; 2.4; 0.056; 0.002; 0.022; 0.001]; % + dot([sqrt(389); sqrt(2.4); sqrt(0.056); sqrt(0.002); sqrt(0.022); sqrt(0.001)], randn(6, 1));
-        v_k = 1e-2 + sqrt(1e-2)*randn(4, 1); %[1e-2; 1e-8; 0.2; 0.3] + sqrt(0.04)*randn(4, 1);
-        w_k = [1e-4; 2e-4; 1e-5; 1e-4; 1e-4; 2e-4]  + sqrt(0.008)*randn(6, 1);
+        x0 = [389*rand(1); 2.4*rand(1); 0.056*rand(1); 0.002*rand(1); 0.022*rand(1); 0.001*rand(1)];
+        v_k = [8*randn(1); 0.2*randn(1); 0.005*randn(1); 0.0002*randn(1); 0.0022*randn(1); 0.0001*randn(1)]; 
 
         [e_k, P_k, ~] = monte_sim_sample(v_k, x0);
 
@@ -16,9 +22,17 @@ function P_k_hat = monte_carlo_simulation()
         P_N(:, :, i) = P_k;
 
         sum = sum + e_N(:, i)*e_N(:, i).';
-        disp("HOE");
     end
-
+    
+   
     % find p_k_hat
     P_k_hat = 1/N*sum;
+
+    
+    sum = 0;
+    for i = 1:N
+        rho_k = e_N(:, i).'*inv(P_N(:, :, i))*e_N(:, i)/(e_N(:, i).'*inv(P_k_hat)*e_N(:, i));
+        sum = sum + log10(rho_k);
+    end
+    nu_k = 10/N*sum;
 end
