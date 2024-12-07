212f244f09b6947c775c449d9b9a4fd592293f6c
Jonah Lovitz <129468112+jlovitz19@users.noreply.github.com>
2024-12-06 21:37:29 -0500

    Update monte_sim_sample.m

--- monte_sim_sample.m
+++ monte_sim_sample.m
@@ -1,5 +1,6 @@
 function [e_k, P_k, y_k] = monte_sim_sample(v_k, x0)
-    % Performs monte carlo simulations
+    % Performs monte carlo simulations for iterative 
+    % Monte Carlo algorithms
     %
     % Parameters:
     % v_k: sample measurement noise
@@ -9,6 +10,8 @@ function [e_k, P_k, y_k] = monte_sim_sample(v_k, x0)
     % Outputs:
     % e_k: sample error
     % P_k: sample error covariance
+    % y_k: sample measurement residual
+
     x0_true = x0;
     
     % first find the true state
@@ -18,7 +21,7 @@ function [e_k, P_k, y_k] = monte_sim_sample(v_k, x0)
         x0_true(3) = 1e-10;
     end
     
-    tint = 0:10;
+    tint = 0:100;
     opts = odeset("RelTol", 1e-3, "AbsTol", 1e-6);
     f = @(t_true, x_true) EoMs(x_true);
     [~, x_true] = ode45(f, tint, x0_true, opts);
@@ -29,7 +32,6 @@ function [e_k, P_k, y_k] = monte_sim_sample(v_k, x0)
     
     % now prepare for kalman filter 
     x0_hat = [400 2 0.048 0.0019 0.027 0.0022].';
-    %P0 = diag([30 0.1 0.09 0.0007 0.018 0.006]); 
     P0 = diag([5 5 5 5 5 5]);
     
     x_k = NaN(6, length(x_true(1, :)));
@@ -37,16 +39,16 @@ function [e_k, P_k, y_k] = monte_sim_sample(v_k, x0)
     
     x_k(:, 1) = x0_hat;
     P_k(:, :, 1) = P0;
+    R_k = ones([6, 6]);
     
     % implementing EKF
     for k = 2:length(x_true(1, :))
-        disp(k);
     
         % prediction step
         [x_k_km1, P_k_km1] = EKF_predict(x_k(:, k - 1), P_k(:, :, k - 1), k);
     
         % update step
-        [x_k_k, P_k_k, y_k] = EKF_update(x_k_km1.', P_k_km1, z_k(:, k), v_k);
+        [x_k_k, P_k_k, y_k] = EKF_update(x_k_km1.', P_k_km1, z_k(:, k), R_k);
     
         x_k(:, k) = x_k_k;
         P_k(:, :, k) = P_k_k;
