c33785b9e890985db1d57cbecad9074011e14d0b
Jonah Lovitz <129468112+jlovitz19@users.noreply.github.com>
2024-12-06 21:30:04 -0500

    Update calc_measurement_jacobian_sym.m

--- calc_measurement_jacobian_sym.m
+++ calc_measurement_jacobian_sym.m
@@ -10,7 +10,12 @@ q_d = x2*cos(x5)*sin(x3) - x1*sin(x5)*sin(x3)*x6 + x1*cos(x5)*cos(x3)*x4;
 y_d = x2*cos(x5)*cos(x3) - x1*sin(x5)*cos(x3)*x6 - x1*cos(x5)*sin(x3)*x4;
 z_d = x2*sin(x5) + x1*cos(x5)*x6;
 
-h_x = (q*q_d + y*y_d + z*z_d)/sqrt(q^2 + y^2 + z^2);
+z2 = (q*q_d + y*y_d + z*z_d)/sqrt(q^2 + y^2 + z^2);
+z4 = (q_d/y - q*y_d/(y^2))/(1 + (q/y)^2);
+z6 = z_d/sqrt(q^2 + y^2) -...
+z/((q^2 + y^2)^(3/2))*(q*y^2*q_d + y*q^2*y_d);
+
+h_x = [x1; z2; x3; z4; x5; x6];
 
 H_x = jacobian(h_x, [x1, x2, x3, x4, x5, x6]);
 disp(H_x);
